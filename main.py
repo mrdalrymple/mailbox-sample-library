1c8c4d59cde78aee584e81bdff52ddbacb1a2f7e
Matthew Dalrymple <matt.dalrymple@gmail.com>
2020-05-16 18:45:07 -0700

    Test basic yapsy plugin

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from yapsy.IPlugin import IPlugin
+
+
+class PluginOne(IPlugin):
+    def print_name(self):
+        print("This is plugin one")
+
 
 def main():
     print("HELLO WORLD FROM MY MODULE")
