874f3acb0a3c65ca3fef7487577627d8ca193bd8
Matthew Dalrymple <matt.dalrymple@gmail.com>
2020-05-17 19:47:04 -0700

    Support on_init()

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ class PluginOne(IPlugin):
     def print_name(self):
         print("This is plugin one")
 
+    def on_init(self, default_api):
+        self.default = default_api
+        self.print_name()
+
 
 def main():
     print("HELLO WORLD FROM MY MODULE")
